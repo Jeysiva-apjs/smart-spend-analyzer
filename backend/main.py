d4a6da22c86df0aafd4aad1efcbbddf624aa1464
JEY <126048586+Jeysiva-apjs@users.noreply.github.com>
2025-01-26 14:47:28 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,6 @@
 import config
 from fastapi import FastAPI
 from routes import crud
-import uvicorn
-
 
 app = FastAPI()
 
@@ -11,6 +9,3 @@ def check_running():
     return {"message": "Server is up and running."}
 
 app.include_router(crud.router)
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
