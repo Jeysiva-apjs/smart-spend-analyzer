ecb7549aea51628a44952c5be7a71f31ae845975
jey <jeysiva.apjs@gmail.com>
2025-01-26 15:37:14 +0530

    updated system path

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,10 @@
-from pathlib import Path
-import sys
 from fastapi import FastAPI
 from routes import crud
-
-parent_dir = str(Path(__file__).resolve().parent.parent)
-print(parent_dir)
-if parent_dir not in sys.path:
-    sys.path.append(parent_dir)
     
-
 app = FastAPI()
 
 @app.get("/")
-def check_running():
+def health_check():
     return {"message": "Server is up and running."}
 
 app.include_router(crud.router)
