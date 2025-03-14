1f22db9d4749c0a45634f953a04a2a19e26dddb6
jey <jeysiva.apjs@gmail.com>
2025-03-14 23:46:32 +0530

    updated frontend

--- frontend/app.py
+++ frontend/app.py
@@ -13,10 +13,11 @@ tab1, tab2, tab3 = st.tabs(["Add/Update", "Analyse by Category", "Analyse by Mon
 with tab1:
     col1, col2 = st.columns([1, 6])  # Adjust column width as needed
     with col1:
-        st.header("Date:")  
+        st.subheader("Date:")  
     with col2:
         selected_date = st.date_input("Enter Date", date.today(), label_visibility="collapsed")
-    st.subheader("Selected Date:", selected_date)
+    # st.subheader("Selected Date:", selected_date)
+
     add_tab(selected_date)
     update_tab(selected_date)
 
