77f343f8bd13f3bd51654881f6ae68b5ab8281ce
jey <jeysiva.apjs@gmail.com>
2025-03-15 00:21:34 +0530

    updated frontend

--- frontend/app.py
+++ frontend/app.py
@@ -11,9 +11,10 @@ st.title("Smart Spend Analyzer")
 tab1, tab2, tab3 = st.tabs(["Add/Update", "Analyse by Category", "Analyse by Month"])
 
 with tab1:
-    col1, col2 = st.columns([1, 9])  # Adjust column width as needed
+    col1, col2 = st.columns([1, 4])  # Adjust column width as needed
     with col1:
-        st.markdown("<p style='font-size: 25px; font-weight: bold;'>Date:</p>", unsafe_allow_html=True)
+        # st.markdown("<p style='font-size: 25px; font-weight: bold;'>Date:</p>", unsafe_allow_html=True)
+        st.subheader('Date:')
 
     with col2:
         selected_date = st.date_input("Enter Date", date.today(), label_visibility="collapsed")
